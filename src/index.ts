9b409784ad855493ad083717655a75361404f315
Anatolii Vershynin <vershinin.dev@gmail.com>
2024-10-01 20:21:16 +0300

    feat: add util checkNumberIsPalindrome

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Utils
-import { sum } from "./utils";
+import { sum, checkNumberIsPalindrome } from "./utils";
 
 main();
 
@@ -7,4 +7,7 @@ function main() {
   console.log("🚀 node-robotdreams app ready");
 
   console.log("sum(10, 20) -> ", sum(10, 20));
+
+  console.log("checkNumberIsPalindrome(212) -> ", checkNumberIsPalindrome(212));
+  console.log("checkNumberIsPalindrome(321) -> ", checkNumberIsPalindrome(321));
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+export const checkNumberIsPalindrome = (value: number): boolean => {
+  const str = value.toString();
+  const reverstedStr = str.split("").reverse().join("");
+  return str === reverstedStr;
+};
